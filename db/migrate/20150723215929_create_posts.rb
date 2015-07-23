7382a6c002b61348663bcc2ab662309075487781
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-07-23 15:07:23 -0700

    first commit

---
+++ db/migrate/20150723215929_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.integer :price
+      t.string :url
+      t.string :type
+
+      t.timestamps null: false
+    end
+  end
+end
