e1c9bbdc91cde84c7194534493d0b59a81d06b5d
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-07-23 15:36:27 -0700

    updated form, index view, new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,37 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.find_by(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def show
+    @post = @post.find(params[:id])
   end
+
+  def destroy
+    @post = @post.find_by(params[:id]).destroy
+  end
+
+  private
+
+  def post_params
+  params.require(:post).permit(:title, :price, :url, :type)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
+
   root 'posts#index'
 
-  get 'new' => 'posts#new'
 
-  get 'edit' => 'posts#edit'
-
-  get 'show' => 'posts#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
